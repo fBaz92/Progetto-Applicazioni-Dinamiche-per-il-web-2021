643216e2842f80e20b6934cb9d7b91f940d8d552
nicolosquarzoni <89456513+nicolosquarzoni@users.noreply.github.com>
2021-09-10 11:38:03 +0200

    risolto errore json in console

--- static/main.js
+++ static/main.js
@@ -58,10 +58,12 @@ var app = new Vue({
                     },
                     body: JSON.stringify(this.date)
                 }
-            );
+                );
             
             var response = await this.loadUpdates();
-            this.updates = await response.json()
+            if (await response) {
+                this.updates = await response.json()    
+            }
         },
 
         async loadUpdates(){
